6806281be52089b9b3cbc76c4e6377662b0ab41a
XeroJCG <xerojcg@gmail.com>
2021-03-12 21:57:23 -0600

    Se sube estructura de maven

---
+++ src/main/java/com/ReinosCenfotecosService/ReinosCenfotecosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ReinosCenfotecosService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinosCenfotecosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinosCenfotecosServiceApplication.class, args);
+	}
+
+}

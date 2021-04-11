6c155094f4061de631cc8893b72665b66e3572e4
XeroJCG <xerojcg@gmail.com>
2021-04-10 21:19:11 -0600

    Update pom exclusion Jackson

--- src/main/java/com/ReinosCenfotecosService/ReinosCenfotecosServiceApplication.java
+++ src/main/java/com/ReinosCenfotecosService/ReinosCenfotecosServiceApplication.java
@@ -2,8 +2,9 @@ package com.ReinosCenfotecosService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class ReinosCenfotecosServiceApplication {
 
     public static void main(String[] args) {

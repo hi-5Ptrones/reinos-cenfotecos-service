b1ff5edc0c9c23277f9cce3dd65289c1df1659d9
XeroJCG <xerojcg@gmail.com>
2021-04-19 19:08:52 -0600

    Se sube validaciones de ataque y correccion de diferentes bugs

--- src/main/java/com/ReinosCenfotecosService/Core/Personajes/RealizarAtaqueEspecial.java
+++ src/main/java/com/ReinosCenfotecosService/Core/Personajes/RealizarAtaqueEspecial.java
@@ -1,19 +1,22 @@
 package com.ReinosCenfotecosService.Core.Personajes;
 
+import com.ReinosCenfotecosService.Core.Partida.GestorAcciones;
+import com.ReinosCenfotecosService.Core.Partida.GestorPartidas;
 import com.ReinosCenfotecosService.Entities.Accion;
 import com.ReinosCenfotecosService.Entities.Casilla;
+import com.ReinosCenfotecosService.Entities.Partida;
 import com.ReinosCenfotecosService.Entities.Personaje;
 import com.ReinosCenfotecosService.exceptions.BussinessException;
 
 public class RealizarAtaqueEspecial extends RealizarAccion{
 
     @Override
-    public void faseDeAccion(Personaje objPersonajeAplica, Personaje objPersonajeAplicar, String[] casillas, String accionRealizar) throws BussinessException {
-        if(accionRealizar == "AtacandoEspecial"){
+    public void faseDeAccion(Personaje objPersonajeAplica, Personaje objPersonajeAplicar, String[] casillas, String accionRealizar, Partida partidaActual, GestorAcciones gesAcciones, int jugadorAplica, int jugadorAplicar) throws Exception {
+        if(accionRealizar.equals("AtacandoEspecial")){
 
         }else{
             if(handler != null){
-                handler.faseDeAccion(objPersonajeAplica, objPersonajeAplicar, casillas, accionRealizar);
+                handler.faseDeAccion(objPersonajeAplica, objPersonajeAplicar, casillas, accionRealizar, partidaActual, gesAcciones, jugadorAplica, jugadorAplicar);
             }else{
                 throw new BussinessException(507);
             }
